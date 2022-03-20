09f3f322df4c97563488ec80e5d9817097405a96
timotius septian <tseptian@student.ciputra.ac.id>
2022-03-21 05:38:14 +0700

    Add project files.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxdata = new System.Windows.Forms.TextBox();
+            this.lbl_inputdata = new System.Windows.Forms.Label();
+            this.lbl_empty = new System.Windows.Forms.Label();
+            this.btn_proses = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxdata
+            // 
+            this.textBoxdata.Location = new System.Drawing.Point(71, 95);
+            this.textBoxdata.Name = "textBoxdata";
+            this.textBoxdata.Size = new System.Drawing.Size(307, 27);
+            this.textBoxdata.TabIndex = 0;
+            // 
+            // lbl_inputdata
+            // 
+            this.lbl_inputdata.AutoSize = true;
+            this.lbl_inputdata.Location = new System.Drawing.Point(71, 53);
+            this.lbl_inputdata.Name = "lbl_inputdata";
+            this.lbl_inputdata.Size = new System.Drawing.Size(94, 20);
+            this.lbl_inputdata.TabIndex = 1;
+            this.lbl_inputdata.Text = "INPUT DATA:";
+            this.lbl_inputdata.Click += new System.EventHandler(this.lbl_inputdata_Click);
+            // 
+            // lbl_empty
+            // 
+            this.lbl_empty.AutoSize = true;
+            this.lbl_empty.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_empty.Location = new System.Drawing.Point(71, 172);
+            this.lbl_empty.Name = "lbl_empty";
+            this.lbl_empty.Size = new System.Drawing.Size(129, 41);
+            this.lbl_empty.TabIndex = 2;
+            this.lbl_empty.Text = "[EMPTY]";
+            // 
+            // btn_proses
+            // 
+            this.btn_proses.Location = new System.Drawing.Point(71, 129);
+            this.btn_proses.Name = "btn_proses";
+            this.btn_proses.Size = new System.Drawing.Size(94, 29);
+            this.btn_proses.TabIndex = 3;
+            this.btn_proses.Text = "Proses";
+            this.btn_proses.UseVisualStyleBackColor = true;
+            this.btn_proses.Click += new System.EventHandler(this.btn_proses_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_proses);
+            this.Controls.Add(this.lbl_empty);
+            this.Controls.Add(this.lbl_inputdata);
+            this.Controls.Add(this.textBoxdata);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxdata;
+        private System.Windows.Forms.Label lbl_inputdata;
+        private System.Windows.Forms.Label lbl_empty;
+        private System.Windows.Forms.Button btn_proses;
+    }
+}
+
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_inputdata_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_proses_Click(object sender, EventArgs e)
+        {
+            if (textBoxdata.Text.Substring(0, 5).ToLower() == "isi: ") 
+            {
+                lbl_empty.Text = textBoxdata.Text.Substring(5);
+            }
+            else if (textBoxdata.Text.ToLower() == "warna: merah")
+            {
+                lbl_empty.ForeColor = Color.Red;
+            }
+            else if (textBoxdata.Text.ToLower() == "warna: default")
+            {
+                lbl_empty.ForeColor = Color.Black;
+            }
+            else if (textBoxdata.Text.ToLower() == "warna: biru")
+            {
+                lbl_empty.ForeColor = Color.Blue;
+            }
+            else if (textBoxdata.Text.ToLower() == "warna: hijau")
+            {
+                lbl_empty.ForeColor = Color.Green;
+            }
+            else if (textBoxdata.Text.ToLower() == "warna: kuning")
+            {
+                lbl_empty.ForeColor = Color.Yellow;
+            }
+            else if (textBoxdata.Text.ToLower() == "restart")
+            {
+                lbl_empty.Text = "[EMPTY]";
+                lbl_empty.ForeColor = Color.Black;
+            }
+            else if (textBoxdata.Text.ToLower() == "kecilkan")
+            {
+                
+            }
+            else if (textBoxdata.Text.ToLower() == "shown")
+            {
+                lbl_empty.Visible = true;
+            }
+            else if (textBoxdata.Text.ToLower() == "hide ")
+            {
+                lbl_empty.Visible = false;
+            }
+            else
+            {
+                lbl_empty.Text = "Syntax error";
+            }
+        }
+    }
+}
